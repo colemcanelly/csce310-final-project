3660b4c83f50228c85898cbb94d6cdfec3905caa
IanBeckett94 <ianbeckett94@tamu.edu>
2023-04-14 20:53:50 -0500

    created html signup page

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@
         <title>log in</title>
 </head>
 <body>
-    <h2>log in</h2>
+    <h2>sign up</h2>
     <div class="login">
     <form>
             <div class="form-group">
@@ -14,8 +14,9 @@
                 <input type="email" id="email">
                 <label for="password">password</label>
                 <input type="password" id="password">
+                <input type="checkbox" name="adminin" value="administrator account">
+                <label>admin</label>
             </div>
-            <input type="submit" value="log in"></input>
             <input type="submit" value ="sign up"></input>
     </form>
 </body>
