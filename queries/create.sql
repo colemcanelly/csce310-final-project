0455f1824a9f044fda9a4411462283d343cd184e
IanBeckett94 <ianbeckett94@tamu.edu>
2023-04-28 15:03:49 -0500

    create query

--- queries/create.sql
+++ queries/create.sql
@@ -1,13 +1,15 @@
+/* needs to follow a certain order so that FK constraints dont prevent drop */
+-- DROP all tables to create new entries
 DROP TABLE
-    user,
+    comment,
     meal_event,
-    food,
     post,
-    comment;
+    food,
+    user;
 
-create table if not exists user
+create table user
 (
-    user_id         int             PRIMARY KEY,
+    user_id         int             PRIMARY KEY         AUTO_INCREMENT,
     first_name      varchar(255)    not null,
     last_name       varchar(255)    not null,
     dob             date            not null,
@@ -16,7 +18,21 @@ create table if not exists user
     account_type    int             not null
 );
 
-create table if not exists meal_event
+create table food
+(
+    food_id         int             PRIMARY KEY         AUTO_INCREMENT,
+    user_id         int,
+    CONSTRAINT `fk_food_user`
+        FOREIGN KEY (user_id) REFERENCES user (user_id)
+        ON DELETE CASCADE
+        ON UPDATE RESTRICT,
+    food_name       varchar(255),
+    calories        int,
+    protein         int,
+    carbs           int
+);
+
+create table meal_event
 (
     event_id        int             PRIMARY KEY,
     meal_date       date,
@@ -33,50 +49,37 @@ create table if not exists meal_event
         ON UPDATE RESTRICT
 );
 
-create table if not exists food
-(
-    food_id         int             PRIMARY KEY         unique,
-    user_id         int,
-    CONSTRAINT `fk_food_user`
-        FOREIGN KEY (user_id) REFERENCES user (user_id)
-        ON DELETE CASCADE
-        ON UPDATE RESTRICT,
-    food_name       varchar(255),
-    calories        int,
-    protein         int,
-    carbs           int
-);
 
-create table if not exists post
+create table post
 (
-    post_id         int           PRIMARY KEY           unique,
+    post_id         int           PRIMARY KEY           AUTO_INCREMENT,
     user_id         int,
     CONSTRAINT `fk_post_user`
         FOREIGN KEY (user_id) REFERENCES user (user_id)
         ON DELETE CASCADE
-        ON UPDATE RESTRICT
-    meal_id         int,
-        CONSTRAINT `fk_post_meal`
-        FOREIGN KEY (meal_id) REFERENCES meal (meal_id)
+        ON UPDATE RESTRICT,
+    food_id         int,
+    CONSTRAINT `fk_post_food`
+        FOREIGN KEY (food_id) REFERENCES food (food_id)
         ON DELETE CASCADE
-        ON UPDATE RESTRICT
+        ON UPDATE RESTRICT,
     post_desc       varchar(255),
     flag            int
 );
 
-create table if not exists comment
+create table comment
 (
-    comment_id      int           PRIMARY KEY           unique,
+    comment_id      int           PRIMARY KEY           AUTO_INCREMENT,
     user_id         int,
         CONSTRAINT `fk_comment_user`
         FOREIGN KEY (user_id) REFERENCES user (user_id)
         ON DELETE CASCADE
-        ON UPDATE RESTRICT
+        ON UPDATE RESTRICT,
     post_id         int,
         CONSTRAINT `fk_comment_post`
         FOREIGN KEY (post_id) REFERENCES post (post_id)
         ON DELETE CASCADE
-        ON UPDATE RESTRICT
+        ON UPDATE RESTRICT,
     emoji           varchar(30),
     comment_text    varchar(255)
 );
