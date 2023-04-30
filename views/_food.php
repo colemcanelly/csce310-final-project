1e9f102ad84d1c5138d9c7ca14ee74eecb1a3b06
margaretzhuang <104533835+margaretzhuang@users.noreply.github.com>
2023-04-29 20:13:41 -0500

    Food form

---
+++ views/_food.php
@@ -0,0 +1,17 @@
+<!--Margaret Zhuang-->
+
+<form action="food.php" method="post">
+  <label for="food-name">Food Name:</label>
+  <input type="text" name="food-name" id="food-name"><br>
+
+  <label for="calories">Calories:</label>
+  <input type="number" name="calories" id="calories"><br>
+
+  <label for="protein">Protein:</label>
+  <input type="number" name="protein" id="protein"><br>
+
+  <label for="carbs">Carbs:</label>
+  <input type="number" name="carbs" id="carbs"><br>
+
+  <input type="submit" value="Publish">
+</form>
