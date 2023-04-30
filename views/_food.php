bcb78af5555271369838be90014df48873daf8c0
margaretzhuang <104533835+margaretzhuang@users.noreply.github.com>
2023-04-30 14:14:03 -0500

    Cannot see success or error messages!?
    Added variables success and error in attempts to display msg, still not working

--- views/_food.php
+++ views/_food.php
@@ -1,5 +1,13 @@
 <!--Margaret Zhuang-->
 
+<?php if(isset($success)): ?>
+  <div class="alert alert-success"><?php echo $success; ?></div>
+<?php endif; ?>
+<?php if(isset($error)): ?>
+  <div class="alert alert-danger"><?php echo $error; ?></div>
+<?php endif; ?>
+
+
 <h2>New Food</h2>
 <form action="food.php" method="post">
 <div class="form-outline mb-4">
@@ -23,10 +31,11 @@
     <input type="text" id="carbs" name="carbs" class="form-control" />
   </div>
   <!-- publish food button -->
-  <button type="submit" name="postFood" value="postFood">publish new food</button>
+  <button type="submit" name="postFood" value="postFood">Publish New Food</button>
 </form>
 
-<!-- food dropdown selects an option from food table -->
+
+<!-- food dropdown selects an option from food table, NOTE: add_post.php does not exist currently -->
 <form action="add_post.php" method="post">
   <div class="form-outline mb-4">
     <select name="foodName">
