8fba3f6bf1397b0a8bf932d19e2a14528fead59f
margaretzhuang <104533835+margaretzhuang@users.noreply.github.com>
2023-04-30 15:00:25 -0500

    correct variable name

--- food.php
+++ food.php
@@ -6,16 +6,16 @@ include_once('layouts/default.php');
 include_once('config.php');
 
 if(isset($_POST['postFood'])){
-  $foodName = mysqli_real_escape_string($conn, $_POST['foodName']);
+  $food_name = mysqli_real_escape_string($conn, $_POST['food_name']);
   $calories = mysqli_real_escape_string($conn, $_POST['calories']);
   $protein = mysqli_real_escape_string($conn, $_POST['protein']);
   $carbs = mysqli_real_escape_string($conn, $_POST['carbs']);
 
   // error check: prompt user to not leave any field blank
-  if(strlen(trim($foodName)) == 0 || strlen(trim($calories)) == 0 || strlen(trim($protein)) == 0 || strlen(trim($carbs)) == 0) {
+  if(strlen(trim($food_name)) == 0 || strlen(trim($calories)) == 0 || strlen(trim($protein)) == 0 || strlen(trim($carbs)) == 0) {
     $error = "All fields are required";
   } else {
-    $query = "INSERT INTO food (food_name, calories, protein, carbs) VALUES ('$foodName', '$calories', '$protein', '$carbs')";
+    $query = "INSERT INTO food (food_name, calories, protein, carbs) VALUES ('$food_name', '$calories', '$protein', '$carbs')";
     if (mysqli_query($conn, $query)) {
       $success = "Food added successfully";
     } else {
