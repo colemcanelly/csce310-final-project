7c38f9e1bf65adb1a9c9bb3bb862bce8cf17c2bb
IanBeckett94 <ianbeckett94@tamu.edu>
2023-04-30 22:13:38 -0500

    user_profile query WIP

--- profile.php
+++ profile.php
@@ -6,20 +6,20 @@
     include_once('config.php');
     include('login_action.php');
 
-    # FIXME unexpected variable "$_SESSION" <-- THIS BREAKS EVERYTHING
     # print user's profile info using the user_profile view 
-    $uid = $_SESSION[user_id];
-    $profile_q = "select uid from user_profile";
+    $fname = $_SESSION['first_name'];
+    $lname = $_SESSION['last_name'];
+    $profile_q = "select * from user_profile where first_name = ".$fname." and last_name = ".$lname."";
     $result = mysqli_query($conn, $profile_q);
 
     # publish new post 
     if (isset($_POST['publishPost'])) {
         # validate post info: food, description
         $post = mysqli_real_escape_string($conn, $_REQUEST['publishPost']);
-        $fid = 
+/*         $fid = 
         $query = "insert into post values
         (NULL, uid, $food_id, , , , )"; # how do we get food_id?
-        $result = mysqli_query($conn, $query) 
+        $result = mysqli_query($conn, $query)  */
     }
 
     # list my posts ordered by reverse id (recent) WIP
