b05079fca645fe1141d6db6a91b5a2d185d96b66
margaretzhuang <104533835+margaretzhuang@users.noreply.github.com>
2023-05-01 23:09:29 -0500

    All msgs show up now

--- food.php
+++ food.php
@@ -14,7 +14,7 @@ if (isset($_POST['postFood'])) {
 
   // error check: prompt user to not leave any field blank
   if (strlen(trim($food_id)) == 0 ||strlen(trim($food_name)) == 0 || strlen(trim($calories)) == 0 || strlen(trim($protein)) == 0 || strlen(trim($carbs)) == 0) {
-    $error = "All fields are required";
+    echo "All fields are required";
   } else {
     // Check if food_id already exists in database
     $existing_food = mysqli_query($conn, "SELECT * FROM food WHERE food_id = '$food_id'");
@@ -22,17 +22,17 @@ if (isset($_POST['postFood'])) {
       // Update existing food item
       $query = "UPDATE food SET food_name='$food_name', calories='$calories', protein='$protein', carbs='$carbs' WHERE food_id='$food_id'";
       if (mysqli_query($conn, $query)) {
-        $success = "Food updated successfully";
+        echo "Food updated successfully";
       } else {
-        $error = "Error updating food: " . mysqli_error($conn);
+        echo "Error updating food: " . mysqli_error($conn);
       }
     } else {
       // Insert new food item
       $query = "INSERT INTO food (food_id, food_name, calories, protein, carbs) VALUES ('$food_id', '$food_name', '$calories', '$protein', '$carbs')";
       if (mysqli_query($conn, $query)) {
-        $success = "Food added successfully";
+        echo "Food added successfully";
       } else {
-        $error = "Error adding food: " . mysqli_error($conn);
+        echo "Error adding food: " . mysqli_error($conn);
       }
     }
   }
